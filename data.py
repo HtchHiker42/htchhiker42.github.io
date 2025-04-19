9de5d502b97f02f896e7fac93e6ead34c261382a
HtchHiker42 <galaxyrgms@gmail.com>
2025-04-19 18:51:34 -0400

    Fix: repaired quote display and submit logic

--- data.py
+++ data.py
@@ -1,5 +1,6 @@
 QUOTES = [
     {"text": "Gallia est omnis divisa in partes tres", "case": "nominative", "use": "subject"},
     {"text": "Veni, vidi, vici", "case": "nominative", "use": "subject"},
-    # Add more quotes!
+    {"text": "Puellae rosam dant", "case": "dative", "use": "indirect object"},
+    {"text": "Romam ire volo", "case": "accusative", "use": "direct object"},
 ]
